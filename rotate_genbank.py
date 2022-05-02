f55a24c8f7c2a4171b8fe8f92ae7b4f8c17e24d3
Joao Ferreira <jf18@sanger.ac.uk>
2022-05-02 19:05:41 +0100

    Simplify shift (rotation) using Biopython slice method on records

--- rotate_genbank.py
+++ rotate_genbank.py
@@ -5,19 +5,16 @@ import sys
 def get_feat_info(feat):
 
     feat_start = feat.location.start
-    feat_end = feat.location.end
-    feat_strand = feat.location.strand
-    feat_len = len(feat.location) 
      
     if "product" in feat.qualifiers:
         feat_name = feat.qualifiers["product"][0]
     elif "gene" in feat.qualifiers:
         feat_name = feat.qualifiers["gene"][0]
     else:
-        print(f"Error. Couldn't find feature name for feature {feat}.")
+        print(f"Error. Couldn't find feature name for feature:\n{feat}.")
         exit(1)
-
-    return (feat_start, feat_end, feat_strand, feat_len, feat_name) 
+    
+    return (feat_start, feat_name)
 
 def rotate_genbank(in_gbk, ref_gene, out_gbk):
     
@@ -25,92 +22,28 @@ def rotate_genbank(in_gbk, ref_gene, out_gbk):
         complete_seq_len = len(seq_record.seq)
         for feat in seq_record.features:
             if feat.type == "gene":
-                feat_start, feat_end, feat_strand, feat_len, feat_name = get_feat_info(feat)
+                feat_start, feat_name = get_feat_info(feat)
                 if feat_name == ref_gene:
-                    ref_feat = feat
-                    ref_start, ref_end, ref_strand, ref_len, ref_name = feat_start, feat_end, feat_strand, feat_len, feat_name
+                    ref_start = feat.location.start
     
-    if ref_start and ref_end and ref_strand and ref_len:
-        #print(ref_len)
-        pass 
-    else:
-        print("Reference gene not found")
+    if not ref_start:
+        print(f"Error. Reference gene {ref_gene} not found")
         sys.exit(1)
-
-    with open(out_gbk, "w") as f: 
-        for seq_record in SeqIO.parse(in_gbk, "genbank"):
-            seq_record.id = f"{seq_record.id}_rotated"
-            seq_record.name = f"{seq_record.name}_rotated"
-            seq_record.description = ""
-            # rotate feature coordinates
-            for feat in seq_record.features:
-
-                feat_start, feat_end, feat_strand, feat_len, feat_name = get_feat_info(feat)
-                
-                if feat_name == ref_name:
-                    new_start = 0
-                    new_end = new_start + feat_len
-                    feat.location = FeatureLocation(start=new_start, end=new_end, strand=feat_strand)
-
-                elif feat_start > ref_start:
-                    new_start = feat_start - ref_start
-                    new_end = new_start + feat_len
-                    feat.location = FeatureLocation(start=new_start, end=new_end, strand=feat_strand)
-                
-                # else if feature comes befores the reference gene 
-                elif feat_start < ref_start:
-                    if feat_end < ref_start:
-                        new_start = ref_len + (complete_seq_len - ref_end) + feat_start 
-                        new_end = new_start + feat_len 
-                        feat.location = FeatureLocation(start=new_start, end=new_end, strand=feat_strand)
-                    else:
-                        print("Warning: feature will possibly be truncated and divided into two features (before and after ORIGIN):")
-                        print(feat)
-                         
-
-#                        # feature before ORIGIN
-#                        new_start = ref_len + (complete_seq_len - ref_end) + feat_start
-#                        new_end = new_start + feat_len
-#                        feat.location = FeatureLocation(start=new_start, end=new_end, strand=feat_strand)
-#                        if "product" in feat.qualifiers:
-#                            feat.qualifiers["product"] = feat.qualifiers["product"][0] + "_beforeOrigin"
-#                        elif "gene" in feat.qualifiers:
-#                            feat.qualifiers["gene"] = feat.qualifiers["gene"][0] + "_beforeOrigin"
-#                        
-#                        # feature after ORIGIN
-#                        overlap_len = feat_end - ref_start
-#                        print(f"overlap_len: {overlap_len}")
-#                        my_feat_location = FeatureLocation(start=0, end=overlap_len, strand=feat_strand)
-#                        my_feat_type = feat.type
-#                        
-#                        if "product" in feat.qualifiers:
-#                            #print("type of feat qualifiers product: ", type(feat.qualifiers["product"]))
-#                            my_feat_name = feat.qualifiers["product"].replace("_beforeOrigin", "_afterOrigin")
-#                            #print(f"my_feat_name: {my_feat_name}")
-#                            #print(f"my_feat_location: {my_feat_location}")
-#                            my_feature = SeqFeature(location=my_feat_location, type=my_feat_type, strand=feat.strand)
-#                            my_feature.qualifiers["product"] = [my_feat_name]
-#                            print("my_feature:")
-#                            print(my_feature)
-#                            seq_record.features.append(SeqFeature(location=my_feat_location, type=my_feat_type, strand=feat.strand))
-#                        elif "gene" in feat.qualifiers:
-#                            #my_feat_name = feat.qualifiers["gene"][0] + "_afterOrigin"
-#                            my_feature = SeqFeature(location=my_feat_location,type=my_feat_type, strand=feat.strand)
-#                            my_feature.qualifiers["gene"] = feat.qualifiers["gene"][0] + "_afterOrigin" 
-#
-#                        #seq_record.features.append(SeqFeature(my_feat_location)) 
-
-            # rotate record sequence
-            before_ref_start = seq_record.seq[0:ref_start]
-            after_ref_start = seq_record.seq[ref_start:]
-            rotated_seq = after_ref_start + before_ref_start
-            seq_record.seq = rotated_seq
-            
-            # write rotated output files 
-            SeqIO.write(seq_record, f, "genbank")
     
-    # generate rotated fasta file
+    # read original record and shift (rotate) it using the 
+    # start of the reference gene as the rotation point 
+    # (i.e. sequence will now start at ref_start)
+    record = SeqIO.read(in_gbk, "genbank")
+    shifted_record = record[ref_start:] + record[:ref_start]
+    
+    shifted_record.id = f"{record.id}_rotated"
+    shifted_record.name = f"{record.name}_rotated"
+    
+    with open(out_gbk, "w") as f:
+        SeqIO.write(shifted_record, f, "genbank")
+
     SeqIO.convert(out_gbk, "genbank", f"{out_gbk}.fasta", "fasta")
+
 
 if __name__ == "__main__":
         
